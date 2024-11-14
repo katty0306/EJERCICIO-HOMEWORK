3529b65b0c2a5a62c0137db8368700f5d38b8662
katty0306 <kattymm635@gmail.com>
2024-11-13 23:20:23 -0500

    UNICO COMMIT

---
+++ EJERCICIO/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Ingrese el tamaño del arreglo: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] arr = new int[n];
+
+Console.WriteLine("Ingrese los " + n + " valores del arreglo:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Valor " + (i + 1) + ": ");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("Ingrese la posición a eliminar (1-" + n + "): ");
+int pos = int.Parse(Console.ReadLine());
+
+if (pos < 1 || pos > n)
+{
+    Console.WriteLine("Posición inválida.");
+    return;
+}
+
+for (int i = pos - 1; i < n - 1; i++)
+{
+    arr[i] = arr[i + 1];
+}
+n--;
+Console.WriteLine("El arreglo después de eliminar el valor en la posición " + pos + " es:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
